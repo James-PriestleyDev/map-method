1749ca65015b0f37c9cb31e40fccd51d429dca16
James Priestley <james.l.priestley@gmail.com>
2024-07-18 11:50:52 +0000

    Map method

--- map.js
+++ map.js
@@ -6,17 +6,34 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
 
+const multByTwo = function(num) {
+  return num * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const simplified = nums.map(function (num) { return num * 2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
 
+const arrow = nums.map(nums => nums * 2);
+console.log(arrow)
 
 // With objects:
+
 const students = [
   {
     id: 1,
@@ -37,3 +54,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id, student.profession]);
+console.log(studentsWithIds);
